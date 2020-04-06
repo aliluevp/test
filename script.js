449dd74966aa95960f8e3e29fca11e0fcc427fa6
pavel <aliluevp@gmail.com>
2020-04-06 13:10:03 +0300

    change

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-'use strict';
-
 let money,
     time;
 money = prompt("Ваш бюджет на месяц?");
 time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
-    budjet : money,
-    timeData :time,
-    expenses : {},
-    optionalExpenses : "",
-    income : [],
-    savings : false
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: "",
+    income: [],
+    savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+let unswer1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    unswer2 = prompt("Во сколько обойдется?"),
+    unswer3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    unswer4 = prompt("Во сколько обойдется?");
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+appData.expenses[unswer1] = unswer2;
+appData.expenses[unswer3] = unswer4;
 
-console.log("статья расходов - "+ a1 + " составляет - " + appData.expenses[a1]);
-console.log("статья расходов - "+ a3 + " составляет - " + appData.expenses[a3]);
+console.log("статья расходов - " + unswer1 + " составляет - " + appData.expenses[unswer1]);
+console.log("статья расходов - " + unswer3 + " составляет - " + appData.expenses[unswer3]);
+
 alert("Ваш бюджет на 1 день - " + appData.budjet / 30);
