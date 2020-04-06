7afdb8a3650283fbd4a1918054994fc58cff9c45
pavel <aliluevp@gmail.com>
2020-04-06 12:41:31 +0300

    задание 1

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 'use strict';
 
-var a = 5;
-console.log(a);
+let money,
+    time;
+money = prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let obj = {
-    name : "kirill",
-    age : 9,
-    male : "man" 
+let appData = {
+    budjet : money,
+    timeData :time,
+    expenses : {},
+    optionalExpenses : "",
+    income : [],
+    savings : false
 };
 
-console.log(obj.name + ' - ' + obj.age);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+console.log("статья расходов - "+ a1 + " составляет - " + appData.expenses[a1]);
+console.log("статья расходов - "+ a3 + " составляет - " + appData.expenses[a3]);
+alert("Ваш бюджет на 1 день - " + appData.budjet / 30);
